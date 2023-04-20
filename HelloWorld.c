87524da9b235bce2cd050978544abf10665ea44b
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-20 07:42:12 -0700

    learned comments and escape sequences

---
+++ HelloWorld.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // this is a comment bloo blah
+
+    /*
+    this is a multi line comment
+    :)
+    */
+
+    /* escape sequences = character combination with a backslash to escape certain characters from their default behavior
+         \n = newline
+         \t = tab
+         \"" = literal text quotes
+    */
+
+    printf("\"This is quite an elegant theater\" - Abraham Lincoln");
+    return 0;
+}
